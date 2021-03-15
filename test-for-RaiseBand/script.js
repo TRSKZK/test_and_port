85b26884c8b9311d5e03433b38841fafb4a5eba4
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-03-15 21:54:04 +0200

    hesponsive header funfctionality

--- test-for-RaiseBand/script.js
+++ test-for-RaiseBand/script.js
@@ -1,1 +1,20 @@
 "use strict";
+const burgerMen = document.querySelector(`.fa-bars`)
+
+const [...navList] = document.querySelectorAll(`.nav-list-link`)
+
+
+// console.log(window.outerWidth);
+
+
+// burgerMen.addEventListener(`click`, () => {
+//     navList.forEach(el => {
+//         if (document.documentElement.scrollWidth < 750) {
+//             if (el.classList.contains(`nav-list-link`))
+//                 el.classList.toggle(`hidden`)
+//         } else el.style.display = `block`
+            
+        
+//     })
+    
+// })
