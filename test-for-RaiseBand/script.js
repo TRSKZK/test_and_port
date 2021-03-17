d2b7927da7ee4b459455eb58973ea2b9a377a7b9
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-03-17 21:04:44 +0200

    fixed nav bar functionality added

--- test-for-RaiseBand/script.js
+++ test-for-RaiseBand/script.js
@@ -1,21 +1,24 @@
 "use strict";
 const burgerMen = document.querySelector(`.nav-burger`);
-const logo = document.querySelector(`.logo`)
+const logo = document.querySelector(`.logo`);
 const navList = document.querySelector(`.nav-list`);
 
 const header = document.querySelector(`header`);
 
-header.addEventListener(`click`, function (e) {
-    e.preventDefault()
-    if (e.target.classList.contains(`nav-burger`))
-    burgerMen.classList.toggle(`active`)
-    navList.classList.toggle(`active`)
-    logo.classList.toggle(`active`)
+document.addEventListener(`click`, function (e) {
+    if (e.target.classList.contains(`nav-burger`)) {
+        burgerMen.classList.toggle(`active`)
+        navList.classList.toggle(`active`)
+        logo.classList.toggle(`active`)
+    }
+    
     console.log(e.target);
 });
 
-
-
 // burgerMen.addEventListener(`click`, function () {
-//    burgerMen.classList.toggle(`active`)
-// })
+//   burgerMen.classList.toggle(`active`);
+//   navList.classList.toggle(`active`);
+//     logo.classList.toggle(`active`);
+    
+//     console.log(this);
+// });
